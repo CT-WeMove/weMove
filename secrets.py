d9fed31bc00f25c28007325e7012780218a3c2fa
Luke Ahn <ahnnuga@gmail.com>
2017-12-07 01:50:57 -0500

    removing secrets.py

--- secrets.py
+++
@@ -1,7 +0,0 @@
-import googlemaps
-
-def get_gmaps():
-    return googlemaps.Client(key='AIzaSyB4JZwzRc8afKdBYCywiqWbGqj_CP3mntc')
-
-def get_database_uri():
-    return 'postgresql+psycopg2://postgres:WeMove@35.185.57.159:5432/wemove_primary'
